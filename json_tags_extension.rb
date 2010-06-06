ae47486a2581746cee10241dcd94ca4d2dfbe75e
Tom Cowell <tc@eightsquarestudio.com>
2010-06-06 01:33:53 +0100

    initial commit with first attempt at code

---
+++ json_tags_extension.rb
@@ -0,0 +1,13 @@
+require 'json'           
+
+class JsonTagExtensionError < StandardError; end
+
+class JsonTagsExtension < Radiant::Extension
+  version "0.0.1"
+  description "Json output tag library"
+  url "http://eightsquarestudio.com/blog/2010/06/05/radiant-json-tags"
+  
+  def activate
+    Page.send :include, JsonTags
+  end
+end
---
+++ lib/json_tags.rb
@@ -0,0 +1,21 @@
+module JsonTags
+  #tags available globally   
+  include Radiant::Taggable
+
+  class JsonTagError < StandardError; end
+  
+  desc %{
+    Usage:
+    <pre><code><r:escape_json>Content Here</r:escape_to_json></code></pre>
+    Escapes the contents inbetween into a json safe format
+  }
+  
+  tag "escape_json" do |tag|
+    if tag.attr["key"]
+      result = { tag.attr["key"] => tag.expand || "" }
+      result.to_json..gsub(/\{|\}/,"")
+    else 
+      ""
+    end
+  end
+end
