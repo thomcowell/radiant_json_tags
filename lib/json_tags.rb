4747dd3afba7f5673ff4f2181495a193ac96b6c0
Tom Cowell <tc@eightsquarestudio.com>
2010-06-12 21:51:00 +0100

    cleaning up escape_json

--- lib/json_tags.rb
+++ lib/json_tags.rb
@@ -6,10 +6,10 @@ module JsonTags
   
   desc %{
     Usage:
-    <pre><code><r:escape_json>Content Here</r:escape_to_json></code></pre>
-    Escapes the contents inbetween into a json safe format
+    <pre><code><r:escape_json key="keyName">Content / Value Here</r:escape_json></code></pre>
+    Escapes the contents inbetween into a json safe format 
+    i.e. 'keyName' : 'Content / Value Here'
   }
-  
   tag "escape_json" do |tag|
     if tag.attr["key"]
       result = { tag.attr["key"] => tag.expand || "" }
