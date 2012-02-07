df19cbdf48c7f7fbcf0389096a7ee6caa301847c
Thomas Cowell <tcowell@eightsquarestudio.com>
2012-02-07 23:21:02 +0000

    fixing json_tags to render large objects and arrays correctly

--- json_tags_extension.rb
+++ json_tags_extension.rb
@@ -1,12 +1,12 @@
-require 'json'           
+require 'json'
 
 class JsonTagExtensionError < StandardError; end
 
 class JsonTagsExtension < Radiant::Extension
-  version "0.0.1"
+  version "0.1.0"
   description "Json output tag library"
   url "http://eightsquarestudio.com/blog/2010/06/05/radiant-json-tags"
-  
+
   def activate
     Page.send :include, JsonTags
   end
--- lib/json_tags.rb
+++ lib/json_tags.rb
@@ -3,114 +3,119 @@ module JsonTags
   include Radiant::Taggable
 
   class JsonTagError < StandardError; end
-  
-  desc %{
-    Usage:
-    <pre><code><r:escape_json key="keyName">Content / Value Here</r:escape_json></code></pre>
-    Escapes the contents inbetween into a json safe format 
-    i.e. 'keyName' : 'Content / Value Here'
-  }
-  tag "escape_json" do |tag|
-    if tag.attr["key"]
-      result = { tag.attr["key"] => tag.expand || "" }
-      result.to_json.gsub(/\{|\}/,"")
-    else 
-      ""
-    end
-  end
-  
+
   desc %{
     Usage:
     <pre><code><r:json_tags>...requires the use of <r:json_tags:*/> inside to build json object...</r:json_tags></code></pre>
     Escapes the contents inbetween into a json safe format from the content added inside
   }
-  tag "json_tags" do |tag| 
-    tag.locals.json_tags = {}
+  tag "json_tags" do |tag|
+    tag.locals.json_tags ||= {}
     tag.expand
-    tag.locals.json_tags.to_json
+    tag.locals.json_tags.to_json if tag.nesting == "content:json_tags"
   end
-  
+
   desc %{
     Usage:
     <pre><code><r:json_tags:pair key="keyName">Content / Value Here</r:json_tags:pair></code></pre>
-    Adds key value pairs to the to_json object which is output in a json safe format  
+    Adds key value pairs to the to_json object which is output in a json safe format
   }
   tag "json_tags:pair" do |tag|
     if tag.attr["key"]
-      tag.locals.json_tags[ tag.attr["key"] ] = tag.expand || ""
+      results = tag.expand
+      tag.locals.json_tags[ tag.attr["key"] ] = (@force ? @results : (@results || results || ""))
+      @results = nil
+      @force = false
     end
+    ""
   end
-  
+
   desc %{
     Usage:
-    <pre><code><r:json_tags:object>...<r:json_tags:pair/>...</r:json_tags:object></code></pre>
-    Adds object to json value  
+    <pre><code><r:json_tags:object key="keyName">...<r:json_tags:pair/>...</r:json_tags:object></code></pre>
+    Adds object to json value, attribute key is optional
   }
   tag "json_tags:object" do |tag|
-    original = tag.locals.json_tags
-    tag.locals.json_tags = {}
-    tag.expand
-    object = tag.locals.json_tags
+	  original = tag.locals.json_tags
+	  tag.locals.json_tags = {}
+	  tag.expand
+    if tag.attr["key"]
+      original[ tag.attr["key"] ] = tag.locals.json_tags
+    else
+       @results = tag.locals.json_tags
+    end
     tag.locals.json_tags = original
-    object
+    ""
   end
-   
+
   desc %{
     Usage:
-    <pre><code><r:json_tags:array>...<r:json_tags:array:item>...</r:json_tags:array></code></pre>
-    Adds array to json value  
+    <pre><code><r:json_tags:array key="keyName">...<r:json_tags:array:item>...</r:json_tags:array></code></pre>
+    Adds array to json value
   }
   tag "json_tags:array" do |tag|
-    original = tag.locals.json_tags_array
-    tag.locals.json_tags_array = []
-    tag.expand
-    array = tag.locals.json_tags_array
-    tag.locals.json_tags_array = original
-    array
+    if tag.attr["key"]
+      original = tag.locals.json_tags_array
+      tag.locals.json_tags_array = []
+      tag.expand
+      tag.locals.json_tags[ tag.attr["key"] ] = tag.locals.json_tags_array
+      tag.locals.json_tags_array = original
+    end
+    ""
   end
-  
+
   desc %{
     Usage:
-    <pre><code><r:json_tags:array:item>Content / Value Here</r:json_tags:array:item></code></pre>
-    Adds key value pairs to the to_json object which is output in a json safe format  
+    <pre><code><r:json_tags:array_item>Content / Value Here</r:json_tags:array_item></code></pre>
+    Pushes the value onto the json array, must be nested inside json_tags:array
   }
-  tag "json_tags:array:item" do |tag|    
-    tag.locals.json_tags_array << tag.expand
+  tag "json_tags:array_item" do |tag|
+    if tag.locals.json_tags_array.is_a? Array
+      results = tag.expand
+      tag.locals.json_tags_array << (@force ? @results : ( @results || results ))
+      @results = nil
+      @force = false
+    end
+    ""
   end
-  
+
   desc %{
     Usage:
     <pre><code><r:json_tags:null/></code></pre>
-    Safe way of adding null json value 
+    Safe way of adding null json value
   }
   tag "json_tags:null" do |tag|
-    nil
+    @force = true
+    @results = nil
   end
-  
+
   desc %{
     Usage:
     <pre><code><r:json_tags:true/></code></pre>
-    Safe way of adding true json value 
+    Safe way of adding true json value
   }
   tag "json_tags:true" do |tag|
-    true
+    @force = true
+    @results = true
   end
-     
+
   desc %{
     Usage:
     <pre><code><r:json_tags:false/></code></pre>
-    Safe way of adding false json value 
+    Safe way of adding false json value
   }
   tag "json_tags:false" do |tag|
-    false
+    @force = true
+    @results = false
   end
-  
+
   desc %{
     Usage:
     <pre><code><r:json_tags:number>23</r:json_tags:number></code></pre>
-    Safe way of adding numbers to json value 
+    Safe way of adding numbers to json value
   }
   tag "json_tags:number" do |tag|
-    tag.expand.to_f
+    @force = true
+    @results = tag.expand.to_f
   end
 end
